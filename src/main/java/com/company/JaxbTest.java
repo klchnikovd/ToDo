49d22dec721f19216ad4b1cc05496a7ad8b4a4f7
Administrator <klchnikovd>
2019-09-20 00:30:50 +0300

    s

---
+++ src/main/java/com/company/JaxbTest.java
@@ -0,0 +1,35 @@
+package src.main.java.com.company;
+
+public class JaxbTest {
+
+    String Task;
+    int id;
+    String caption;
+    String Description;
+
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(String name) {
+        this.id = id;
+    }
+
+    public String getcaption() {
+        return caption;
+    }
+
+    public void setcaption() {
+        this.caption = caption;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setId() {
+        this.Description = Description;
+    }
+
+}
